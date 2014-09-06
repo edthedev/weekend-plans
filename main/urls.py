aca13ebf16ef09a2ec85cc4154f865dbcee9faa5
Edward Delaporte <edthedev@gmail.com>
2014-09-06 12:03:51 -0500

    Moved a copy of the add form to list view.

--- main/urls.py
+++ main/urls.py
@@ -3,14 +3,14 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from weekend.views import ListView, CreateView, EditView, DeleteView
+from weekend.views import ListPlans, CreatePlan, EditPlan, DeletePlan
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', ListView.as_view(), name='list_plans'),
-    url(r'^create/$', CreateView.as_view(), name='create_plan'),
-    url(r'^edit/(?P<pk>\d+)/$', EditView.as_view(), name='e_plan'),
-    url(r'^finish/(?P<pk>\d+)/$', DeleteView.as_view(), name='delete_plan'),
+    url(r'^$', ListPlans.as_view(), name='list_plans'),
+    url(r'^create/$', CreatePlan.as_view(), name='create_plan'),
+    url(r'^edit/(?P<pk>\d+)/$', EditPlan.as_view(), name='e_plan'),
+    url(r'^finish/(?P<pk>\d+)/$', DeletePlan.as_view(), name='delete_plan'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
--- weekend/views.py
+++ weekend/views.py
@@ -3,24 +3,37 @@
 # Create your views here.
 
 from django.core.urlresolvers import reverse_lazy
+from django.forms import ModelForm
+from django.views.generic.edit import FormMixin
+
 from vanilla import CreateView, DeleteView, ListView, UpdateView
 
 from weekend.models import WeekendPlan
 
-class ListView(ListView):
+class CreateForm(ModelForm):
+    class Meta:
+        model = WeekendPlan
+        # fields = ['what_to_do']
+
+class ListPlans(ListView):
     model = WeekendPlan
 
+    def get_context_data(self, **kwargs):
+        context = super(ListPlans, self).get_context_data(**kwargs)
+        context['form'] = CreateForm
+        return context
 
-class CreateView(CreateView):
+class CreatePlan(CreateView):
+    model = WeekendPlan
+    success_url = reverse_lazy('list_plans')
+    form_class = CreateForm
+
+
+class EditPlan(UpdateView):
     model = WeekendPlan
     success_url = reverse_lazy('list_plans')
 
 
-class EditView(UpdateView):
-    model = WeekendPlan
-    success_url = reverse_lazy('list_plans')
-
-
-class DeleteView(DeleteView):
+class DeletePlan(DeleteView):
     model = WeekendPlan
     success_url = reverse_lazy('list_plans')
