8f0a7e2ed18a5a23a39dde1de8a20f4b098f793e
Edward Delaporte <edthedev@gmail.com>
2014-09-06 09:48:25 -0500

    Better appearance.

--- main/urls.py
+++ main/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^$', ListView.as_view(), name='list_plans'),
     url(r'^create/$', CreateView.as_view(), name='create_plan'),
     url(r'^edit/(?P<pk>\d+)/$', EditView.as_view(), name='e_plan'),
-    url(r'^delete/(?P<pk>\d+)/$', DeleteView.as_view(), name='delete_plan'),
+    url(r'^finish/(?P<pk>\d+)/$', DeleteView.as_view(), name='delete_plan'),
     url(r'^admin/', include(admin.site.urls)),
 )
 
