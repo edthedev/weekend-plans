1597fee8129e8f1e9260c5a461e452acaf173455
Edward Delaporte <edthedev@gmail.com>
2014-09-14 08:42:18 -0500

    URL routing for shopping app

---
+++ shopping/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from shopping.views import ListPlanToBuy
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', ListPlanToBuy.as_view(), name='shopping_list'),
+    url(r'^complete_a_plan/(?P<pk>\d+)/$', ListPlanToBuy.as_view(), name='shopping_list'),
+    #url(r'^completed$', ListCompletedPlans.as_view(), name='completed_plans'),
+    #url(r'create/$', CreatePlan.as_view(), name='create_plan'),
+    #url(r'edit/(?P<pk>\d+)/$', EditPlan.as_view(), name='edit_plan'),
+)
