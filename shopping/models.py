14ef4875b51960dcf412adba28e47a86d7f96a4e
Edward Delaporte <edthedev@gmail.com>
2014-09-14 08:33:06 -0500

    Added app for tracking planned expenses.
    (In order to do 30 day delay when planning to buy something...)

---
+++ shopping/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+import datetime
+
+class PlanToBuy(models.Model):
+    ''' Contains the weekend plans. '''
+    added = models.DateTimeField(
+            default=datetime.now,
+            )
+    name = models.CharField(
+            max_length = 200,
+            unique=True,
+            )
+    plan_to_buy = models.DateTimeField(
+            default=None,
+            blank=True,
+            null=True,
+            )
+    bought = models.DateTimeField(
+            default=None,
+            blank=True,
+            null=True,
+            )
