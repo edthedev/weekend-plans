61e8fb1a1c5ae043248c20e4ed38b70ce4fa234b
Edward Delaporte <edthedev@gmail.com>
2014-09-13 22:53:35 -0500

    Moved weekend urls into inside of app.
    (Refactor)

---
+++ weekend/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, url
+
+from weekend.views import ListPlans, CreatePlan, EditPlan, ListCompletedPlans
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', ListPlans.as_view(), name='list_plans'),
+    url(r'^complete_a_plan/(?P<pk>\d+)/$', ListPlans.as_view(), name='list_plans'),
+    url(r'^completed$', ListCompletedPlans.as_view(), name='completed_plans'),
+    url(r'create/$', CreatePlan.as_view(), name='create_plan'),
+    url(r'edit/(?P<pk>\d+)/$', EditPlan.as_view(), name='edit_plan'),
+)
