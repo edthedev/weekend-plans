06a52a66df318ac98e6e1cb66cfc028d84555534
Edward Delaporte <edthedev@gmail.com>
2014-10-19 11:37:51 -0500

    Added price field.

--- shopping/models.py
+++ shopping/models.py
@@ -10,6 +10,11 @@ class PlanToBuy(models.Model):
             max_length = 200,
             unique=True,
             )
+    price = models.DecimalField(
+            decimal_places=2,
+            max_digits=8,
+            blank=True,
+            null=True)
     expected_purchase_date = models.DateTimeField(
             default=None,
             blank=True,
