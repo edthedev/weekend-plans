bd15645299fca81d7b89025fbb6eab1c6f753a70
Edward Delaporte <edthedev@gmail.com>
2014-10-28 13:14:51 -0500

    Creating new AddListView.

--- shopping/views.py
+++ shopping/views.py
@@ -12,27 +12,31 @@ class AddListView(ListView):
     model = None
     template_name = None
     queryset = None
-    url = None
 
     def post(self, request, *args, **kwargs):
-        ''' Special handling for 'complete' action. '''
-        if 'Completed' in request.POST['action']:
-            pk = kwargs['pk']
-            plan = self.model.objects.get(pk=pk)
-            plan.completed = datetime.now()
-            plan.save()
-        if 'Delete' in request.POST['action']:
-            pk = kwargs['pk']
-            plan = self.model.objects.get(pk=pk)
-            plan.delete()
-        return redirect(self.url)
+        ''' Special handling for different forms. '''
+#        if 'Add' in request.POST['action']:
+#            # TODO: Do the default stuff here.
+#            pk = kwargs['pk']
+#            plan = self.model.objects.get(pk=pk)
+#            plan.save()
+#        if 'Completed' in request.POST['action']:
+#            pk = kwargs['pk']
+#            plan = self.model.objects.get(pk=pk)
+#            plan.completed = datetime.now()
+#            plan.save()
+#        if 'Delete' in request.POST['action']:
+#            pk = kwargs['pk']
+#            plan = self.model.objects.get(pk=pk)
+#            plan.delete()
+        return self.get(request, *args, **kwargs)
+        # return redirect(self)
 
     def get_context_data(self, **kwargs):
         ''' Include the edit for below. '''
         context = super(AddListView, self).get_context_data(**kwargs)
         context['form'] = self.create_form
         return context
-
 
 class CreateForm(ModelForm):
     class Meta:
@@ -44,25 +48,3 @@ class ShoppingListView(AddListView):
         PlanToBuy.objects.filter(bought__isnull=True).order_by('added', 'name')
     url = 'shopping'
     create_form = CreateForm
-
-class ListPlanToBuy(ListView):
-    ''' Show the list of non-completed plans. '''
-    template_name = 'shopping/shopping_list.html'
-    queryset = \
-        PlanToBuy.objects.filter(bought__isnull=True).order_by('added', 'name')
-
-    def post(self, request, *args, **kwargs):
-        ''' Special handling for 'Bought' action. '''
-        if 'Bought' in request.POST['action']:
-            pk = kwargs['pk']
-            plan = PlanToBuy.objects.get(pk=pk)
-            plan.bought = datetime.now()
-            plan.save()
-        return redirect('list_plans')
-
-    def get_context_data(self, **kwargs):
-        ''' Include the edit for below. '''
-        context = super(ListPlanToBuy, self).get_context_data(**kwargs)
-        context['form'] = CreateForm
-        return context
-
