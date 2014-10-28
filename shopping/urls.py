7b0204230ecb486839f7b067393705eb6e7bfc77
Edward Delaporte <edthedev@gmail.com>
2014-10-28 17:21:56 -0500

    Random URL changes.

--- shopping/urls.py
+++ shopping/urls.py
@@ -4,7 +4,7 @@ from shopping.views import ShoppingListView
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', ShoppingListView.as_view(), name='shopping_list'),
+    url(r'^$', ShoppingListView.as_view()),
 #    url(r'^complete_a_plan/(?P<pk>\d+)/$', ListPlanToBuy.as_view(), name='shopping_list'),
     #url(r'^completed$', ListCompletedPlans.as_view(), name='completed_plans'),
     #url(r'create/$', CreatePlan.as_view(), name='create_plan'),
