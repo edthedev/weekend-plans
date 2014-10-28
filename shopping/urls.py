17cf82e193c6b0847a7940c3bc917262b1b888b4
Edward Delaporte <edthedev@gmail.com>
2014-10-28 13:04:56 -0500

    Form displays correctly anyway.

--- shopping/urls.py
+++ shopping/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, url
 
-from shopping.views import ListPlanToBuy
+from shopping.views import ShoppingListView
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', ListPlanToBuy.as_view(), name='shopping_list'),
-    url(r'^complete_a_plan/(?P<pk>\d+)/$', ListPlanToBuy.as_view(), name='shopping_list'),
+    url(r'^$', ShoppingListView.as_view(), name='shopping_list'),
+#    url(r'^complete_a_plan/(?P<pk>\d+)/$', ListPlanToBuy.as_view(), name='shopping_list'),
     #url(r'^completed$', ListCompletedPlans.as_view(), name='completed_plans'),
     #url(r'create/$', CreatePlan.as_view(), name='create_plan'),
     #url(r'edit/(?P<pk>\d+)/$', EditPlan.as_view(), name='edit_plan'),
--- shopping/views.py
+++ shopping/views.py
@@ -3,22 +3,47 @@ from django.shortcuts import redirect
 from django.forms import ModelForm
 from django.views.generic.list import ListView
 
-from datetimewidget.widgets import DateWidget
-
 from shopping.models import PlanToBuy
 
 from datetime import datetime
 
+class AddListView(ListView):
+    ''' Show the list of non-completed plans. '''
+    model = None
+    template_name = None
+    queryset = None
+    url = None
+
+    def post(self, request, *args, **kwargs):
+        ''' Special handling for 'complete' action. '''
+        if 'Completed' in request.POST['action']:
+            pk = kwargs['pk']
+            plan = self.model.objects.get(pk=pk)
+            plan.completed = datetime.now()
+            plan.save()
+        if 'Delete' in request.POST['action']:
+            pk = kwargs['pk']
+            plan = self.model.objects.get(pk=pk)
+            plan.delete()
+        return redirect(self.url)
+
+    def get_context_data(self, **kwargs):
+        ''' Include the edit for below. '''
+        context = super(AddListView, self).get_context_data(**kwargs)
+        context['form'] = self.create_form
+        return context
+
+
 class CreateForm(ModelForm):
     class Meta:
         model = PlanToBuy
-        widgets = {
-#Use localization and bootstrap 3
-            'datetime': DateWidget(
-                attrs={'id':"yourdatetimeid"},
-                usel10n = True,
-                bootstrap_version=3)
-        }
+
+class ShoppingListView(AddListView):
+    template_name = 'shopping/shopping_list.html'
+    queryset = \
+        PlanToBuy.objects.filter(bought__isnull=True).order_by('added', 'name')
+    url = 'shopping'
+    create_form = CreateForm
 
 class ListPlanToBuy(ListView):
     ''' Show the list of non-completed plans. '''
