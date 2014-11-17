8ce4d57f73d2536f2784186a53600366a2e3f66c
Edward Delaporte <edthedev@gmail.com>
2014-11-16 20:02:53 -0600

    Path correction to match common checkout.

--- main/wsgi.py
+++ main/wsgi.py
@@ -7,7 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weekend.main.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "weekend-plans.main.settings")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
